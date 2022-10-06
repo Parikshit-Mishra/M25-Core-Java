681f3e19e47836a70b3b591640dec93df7ada4da
Namrata Mishra <namratapande111@gmail.com>
2022-10-06 17:39:13 +0530

    First Commit

---
+++ Day1_EclipesConnectWithGitHub/src/orgtncindiaBasic/EclipseConnectGitHub.java
@@ -0,0 +1,10 @@
+package orgtncindiaBasic;
+
+public class EclipseConnectGitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome to New JAVA World");
+	}
+
+}
