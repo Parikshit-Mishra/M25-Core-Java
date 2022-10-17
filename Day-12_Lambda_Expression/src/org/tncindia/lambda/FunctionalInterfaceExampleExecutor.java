d4ff103c033c6f530a85e7b246bca1c66f0743e7
Namrata Mishra <namratapande111@gmail.com>
2022-10-17 21:40:56 +0530

    Commit

---
+++ Day-12_Lambda_Expression/src/org/tncindia/lambda/FunctionalInterfaceExampleExecutor.java
@@ -0,0 +1,17 @@
+package org.tncindia.lambda;
+
+public class FunctionalInterfaceExampleExecutor {
+	public static void main(String[] args) {
+		FunctionalInterfaceExample obj=(int a,int b)->
+		{
+			int result=(a>b)?a:b;
+			
+			//System.out.println("The maximum number is:" +result);
+			return result;
+			
+		};
+		System.out.println("The maximum number is:" +obj.max(34, 67));
+
+	}
+
+}
